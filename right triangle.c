f340b73cc8bc295088e9830e328808106bfe656e
Gitanjali2001 <69495628+Gitanjali2001@users.noreply.github.com>
2020-08-18 23:21:42 +0530

    Add files via upload

---
+++ right triangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j;
+	for(i=0;i<=8;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+			if((i+j)%2==0)
+			{
+				printf("1");
+				
+			}
+			else
+			{
+				printf("0");
+				
+			}
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
